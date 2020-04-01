ef9417b5016e1f22147cd68dbf8f46a114a06aec
vezeveer <mortalmanny@live.com>
2020-04-01 16:08:48 +0800

    initial

---
+++ globals/header.php
@@ -0,0 +1,5 @@
+<div class="">
+  <h5> Copyright 2020 EV </h5>
+</div>
+
+</body>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <!DOCTYPE html>
+<html>
+
+<?php include 'globals/header.php' ?>
+
+<p>Section Area</p>
+
+<?php include 'globals/footer.php' ?>
 
 </html>
